9eca7979316a9f4ac8cccf9b23a8e5da65c16a70
Dhyey <dhyeypf@gmail.com>
2025-08-31 18:59:06 +0530

    ready to deploy

---
+++ src/layout/app-layout.jsx
@@ -0,0 +1,20 @@
+import Header from "@/components/header";
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <div className="grid-background"></div>
+      <main className="min-h-screen container pr-3 pl-3">
+        <Header />
+        <Outlet />
+      </main>
+      <div className="p-10 text-center bg-gray-800 mt-10">
+        Made by Dhyey Fultariya
+      </div>
+    </div>
+  );
+};
+
+export default AppLayout;
